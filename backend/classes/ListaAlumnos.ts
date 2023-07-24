33e12b4b32b88bbb48b86ddd51bc17d81106ea25
Quico Saval <quicosv@gmail.com>
2023-07-24 19:33:38 +0200

    Finaliza las clases del backend
    Co-authored-by: Dinoscor software <dinoscor@users.noreply.github.com>

---
+++ backend/classes/ListaAlumnos.ts
@@ -0,0 +1,19 @@
+import { Alumno } from "./Alumno";
+
+export class ListaAlumnos {
+	private alumnos: Alumno[];
+	constructor() {
+		this.alumnos = [];
+	}
+
+	addAlumno(nombre: string, notaMates: number, notaLengua: number, notaHistoria: number): void {
+		if (!this.alumnos.find((x) => x.nombre === nombre)) {
+			const nuevoAlumno = new Alumno(this.alumnos.length + 1, nombre, notaMates, notaLengua, notaHistoria);
+			this.alumnos.push(nuevoAlumno);
+		}
+	}
+
+	getAlumnos(): Alumno[] {
+		return this.alumnos;
+	}
+}
