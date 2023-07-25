c434d373771d84f09f52bee07817c7c1e1f30261
Quico Saval <quicosv@gmail.com>
2023-07-25 18:37:21 +0200

    Reescribe la importación de bootstrap en el frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
