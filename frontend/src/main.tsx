199314c4cb2b4e7d1c7774efed4014db01692397
Quico Saval <quicosv@gmail.com>
2023-07-28 19:01:48 +0200

    Corrige la importación de nuestro css en main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'niapa.css'
+import './niapa.css'
 import { Notas } from './Notas'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
